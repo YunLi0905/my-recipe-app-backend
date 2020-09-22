13a2965ded890b995747d279ecd353058bfc3cce
Yun Li <liyun.aino@icloud.com>
2020-09-22 18:50:13 +0300

    made the backend work with the frontend

---
+++ build/precache-manifest.c691b0c3e793432ba3c2fc00b445ebc7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d55e796224815890fe9cc433d12778ca",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a53f66763a5ebe8258e2",
+    "url": "/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "4473c6634b642fe2fa67",
+    "url": "/static/js/2.e7c60f69.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.e7c60f69.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a53f66763a5ebe8258e2",
+    "url": "/static/js/main.07eb9c3f.chunk.js"
+  },
+  {
+    "revision": "73635f645c271e35ff15",
+    "url": "/static/js/runtime-main.83c3e0c4.js"
+  }
+]);
